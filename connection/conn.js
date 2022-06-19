91dafb7ba78c1382162c0a89243a64204804c85e
olawalemayor <olawalemayor90@gmail.com>
2022-06-19 21:19:54 +0100

    added config and readme

--- connection/conn.js
+++ connection/conn.js
@@ -1,10 +1,20 @@
 const mongoose = require("mongoose");
+const config = require("config");
 
-const dbURL = "localhost:27017";
+//database Options
+const dbUser = config.get("dbUser");
+const dbPass = config.get("dbPass");
+const dbURL =
+  dbUser && dbPass
+    ? `${dbUser}:${dbPass}@${config.get("dbUrl")}`
+    : config.get("dbUrl");
+const dbPort = config.get("dbPort");
+const dbName = config.get("dbName");
 
+//connect method
 const connectApptoDb = () => {
   return mongoose
-    .connect(`mongodb://${dbURL}/crud-app`)
+    .connect(`mongodb://${dbURL}:${dbPort}/${dbName}`)
     .then(() => {
       console.log("Connected to database!!!");
     })
---
+++ readme.md
@@ -0,0 +1,25 @@
+# Simple CRUD APP by Olawale Mayor
+
+Built using Mongoose, Node and Express
+
+## Database variables
+
+Create configuration JSON files for developement and production environment in the config folder as
+default.json - For development
+production.json - For production
+
+```json
+{
+    "dbUrl": "DATABASE HOST",
+  "dbPort": 27017, //DATABASE PORT
+  "dbName": "DATABASE NAME",
+  "dbUser": "DATABASE USERNAME",
+  "dbPass": "DATABASE PASSWORD"
+}
+```
+
+## To start the server
+
+```bash
+npm start
+```
